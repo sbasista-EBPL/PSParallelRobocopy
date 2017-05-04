d8b654cc141fcb6204307b859e35eb9fd86f97be
sbasista-EBPL <sbasista@ebpl.org>
2017-05-03 20:56:35 -0400

    Start of script
    PS workflow script creates parallel robocopy instances.

---
+++ PSParallelRobocopy.ps1
@@ -0,0 +1,14 @@
+﻿$computers = 'somerandomcomputer'
+
+workflow testrobocopy {
+    param([string[]]$computers)
+
+    $CurrentDate = Get-Date -Format "yyyy-MM-dd"
+
+    foreach -parallel ($server in $computers) {
+
+        robocopy "local source" "\\$server\c$\destination directory" /e /copyall /MIR /SEC /SECFIX /NP /TEE /R:4 /W:30 /MT:32 /LOG+:"C:\Logs\$server($CurrentDate).txt"
+
+        }
+
+    }
